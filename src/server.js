33ed9ad32e4a2eebb69d0e12cb97693e9d0d71df
kom50 <82828406+kom50@users.noreply.github.com>
2021-12-04 22:12:13 +0530

    some change

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORt || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
